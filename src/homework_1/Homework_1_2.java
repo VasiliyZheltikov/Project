2417f98fc7b7c7f20d109805dc9e5d43802ba9f4
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-11 19:17:10 +0300

    Homework_1_2.java DONE

--- src/homework_1/Homework_1_2.java
+++ src/homework_1/Homework_1_2.java
@@ -1,4 +1,14 @@
 package homework_1;
 
 public class Homework_1_2 {
+
+    public static void main(String[] args) {
+
+        int n = 47;
+
+        int d = n / 10; // получение десятков
+        int e = n % 10; // получение единиц
+
+        System.out.println(d + e);
+    }
 }
