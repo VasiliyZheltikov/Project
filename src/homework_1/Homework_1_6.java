90d744e78e6a9c1282b95fc14dcc52ff57f6bb6b
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-11 20:33:00 +0300

    Homework_1_6.java DONE

--- src/homework_1/Homework_1_6.java
+++ src/homework_1/Homework_1_6.java
@@ -1,4 +1,18 @@
 package homework_1;
 
 public class Homework_1_6 {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        int memory_a = a;
+
+        a = b;
+        b = memory_a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
