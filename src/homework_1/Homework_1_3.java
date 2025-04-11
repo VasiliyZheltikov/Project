50c07fdeb90c2e3eb2750bb4b62506446da22b2a
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-11 19:22:58 +0300

    Homework_1_3.java DONE

--- src/homework_1/Homework_1_3.java
+++ src/homework_1/Homework_1_3.java
@@ -1,4 +1,15 @@
 package homework_1;
 
 public class Homework_1_3 {
+
+    public static void main(String[] args) {
+
+        int n = 197; // число от 100 до 999
+
+        int s = n / 100; // число сотен
+        int d = n / 10 % 10; // число десятков
+        int e = n % 10; // число единиц
+
+        System.out.println(s + d + e);
+    }
 }
