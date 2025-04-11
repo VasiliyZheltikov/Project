3de023739847b941af282411c5ed9050d22ade29
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-11 19:56:46 +0300

    Homework_1_4.java DONE

--- src/homework_1/Homework_1_4.java
+++ src/homework_1/Homework_1_4.java
@@ -1,4 +1,13 @@
 package homework_1;
 
 public class Homework_1_4 {
+
+    public static void main(String[] args) {
+
+        double n = 14.51;
+
+        int m = (int)Math.round(n);
+
+        System.out.println(m);
+    }
 }
