f4fee2cceff18d791da9a51a9dae0e126d9c4901
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-11 18:44:19 +0300

    Homework_1_1.java DONE

--- src/homework_1/Homework_1_1.java
+++ src/homework_1/Homework_1_1.java
@@ -1,4 +1,12 @@
 package homework_1;
 
 public class Homework_1_1 {
+
+    public static void main(String[] args) {
+
+        int b = 5;
+        int c = 3;
+
+        System.out.println("a = " + 4 * (b + c - 1) / 2);
+    }
 }
