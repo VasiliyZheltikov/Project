543cce4b8f24003bb47046d4dc662ab1c2a3c6d3
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-15 20:47:12 +0300

    Homework_2_2.java DONE

--- src/homework_2/Homework_2_2.java
+++ src/homework_2/Homework_2_2.java
@@ -1,4 +1,21 @@
 package homework_2;
 
+import java.util.Scanner;
+
 public class Homework_2_2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        if (t > -5) {
+            System.out.println("Warm");
+        } else if (t <= -5) {
+            if (t > -20) {
+                System.out.println("Normal");
+            } else if (t <= -20) {
+                System.out.println("Cold");
+            }
+        }
+        scanner.close();
+    }
 }
