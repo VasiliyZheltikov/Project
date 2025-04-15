5f7efec6bb749c43687c033f5ad5b9ed40e11c16
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-15 21:03:25 +0300

    Revert "Homework_2_3.java DONE"
    This reverts commit aee8580aa5d18dd7f31de215845b6e7eee748b7a.

--- src/homework_2/Homework_2_4.java
+++ src/homework_2/Homework_2_4.java
@@ -1,12 +1,4 @@
 package homework_2;
 
 public class Homework_2_4 {
-
-    public static void main(String[] args) {
-        int i = 7;
-        while (i < 99) {
-            System.out.print(i + " ");
-            i += 7;
-        }
-    }
 }
