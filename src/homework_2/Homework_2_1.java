cccdeb90f7f296d19b10dd03d5156b42692093ae
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-15 20:04:13 +0300

    Homework_2_1.java DONE

--- src/homework_2/Homework_2_1.java
+++ src/homework_2/Homework_2_1.java
@@ -1,4 +1,17 @@
 package homework_2;
 
+import java.util.Scanner;
+
 public class Homework_2_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Число " + number + " чётное");
+        } else {
+            System.out.println("Число " + number + " нечётное");
+        }
+        scanner.close();
+    }
 }
