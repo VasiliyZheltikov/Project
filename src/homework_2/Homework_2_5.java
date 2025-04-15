ef6641213d41dd421e9397167d1d3c78c2fa98ed
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-15 21:26:47 +0300

    Homework_2_5.java DONE

--- src/homework_2/Homework_2_5.java
+++ src/homework_2/Homework_2_5.java
@@ -1,4 +1,23 @@
 package homework_2;
 
+import java.util.Scanner;
+
 public class Homework_2_5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        int summand = 1;
+        int result = 0;
+        if (i > 0) {
+            while (summand <= i) {
+             result += summand;
+             summand++;
+            }
+            System.out.println(result);
+        } else {
+            System.out.println("Введите целое число больше 0");
+        }
+        scanner.close();
+    }
 }
