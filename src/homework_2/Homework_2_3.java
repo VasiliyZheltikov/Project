febc475807fab88f3b73ea62ee5d3cf5cb78747b
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-15 20:54:37 +0300

    Homework_2_3.java DONE

--- src/homework_2/Homework_2_3.java
+++ src/homework_2/Homework_2_3.java
@@ -1,4 +1,13 @@
 package homework_2;
 
 public class Homework_2_3 {
+
+    public static void main(String[] args) {
+        int i = 10;
+        while (i < 21) {
+            int result = i * i;
+            System.out.print(result + " ");
+            i++;
+        }
+    }
 }
