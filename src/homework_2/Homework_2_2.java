6603a6a58ae2a1a06f1cc33500622d8344c021ad
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-04-17 23:00:16 +0300

    Homework_2_2.java, Homework_2_5.java EDITED (based on comments in pull request #2)

--- src/homework_2/Homework_2_2.java
+++ src/homework_2/Homework_2_2.java
@@ -6,15 +6,14 @@ public class Homework_2_2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите значение температуры: ");
         int t = scanner.nextInt();
         if (t > -5) {
             System.out.println("Warm");
-        } else if (t <= -5) {
-            if (t > -20) {
-                System.out.println("Normal");
-            } else if (t <= -20) {
-                System.out.println("Cold");
-            }
+        } else if (t > -20) {
+            System.out.println("Normal");
+        } else {
+            System.out.println("Cold");
         }
         scanner.close();
     }
--- src/homework_2/Homework_2_5.java
+++ src/homework_2/Homework_2_5.java
@@ -6,21 +6,26 @@ public class Homework_2_5 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        if (scanner.hasNextInt()) {
-            int i = scanner.nextInt();
-            int summand = 1;
-            int result = 0;
-            if (i > 0) {
-                while (summand <= i) {
-                    result += summand;
-                    summand++;
+        System.out.print("Введите целое число больше 0: ");
+        while (true) {
+            if (scanner.hasNextInt()) {
+                int i = scanner.nextInt();
+                int summand = 1;
+                int result = 0;
+                if (i > 0) {
+                    while (summand <= i) {
+                        result += summand;
+                        summand++;
+                    }
+                    System.out.println("Сумма чисел от 1 до введённого числа = " + result);
+                    break;
+                } else {
+                    System.out.print("Ошибка: введите целое число БОЛЬШЕ 0: ");
                 }
-                System.out.println(result);
             } else {
-                System.out.println("Введите целое число больше 0");
+                System.out.print("Ошибка: введите ЦЕЛОЕ ЧИСЛО больше 0: ");
+                scanner.next();
             }
-        } else {
-            System.out.println("Введите целое число больше 0");
         }
         scanner.close();
     }
