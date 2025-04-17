b826ad882c6b5428d46d14f2c606d0c55189637b
Василий Жёлтиков <108517372+VasiliyZheltikov@users.noreply.github.com>
2025-04-17 17:09:35 +0300

    Added homework_2 (#2)

---
+++ src/homework_2/Homework_2_4.java
@@ -0,0 +1,12 @@
+package homework_2;
+
+public class Homework_2_4 {
+
+    public static void main(String[] args) {
+        int i = 7;
+        while (i < 99) {
+            System.out.print(i + " ");
+            i += 7;
+        }
+    }
+}
---
+++ src/homework_2/Homework_2_5.java
@@ -0,0 +1,27 @@
+package homework_2;
+
+import java.util.Scanner;
+
+public class Homework_2_5 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int i = scanner.nextInt();
+            int summand = 1;
+            int result = 0;
+            if (i > 0) {
+                while (summand <= i) {
+                    result += summand;
+                    summand++;
+                }
+                System.out.println(result);
+            } else {
+                System.out.println("Введите целое число больше 0");
+            }
+        } else {
+            System.out.println("Введите целое число больше 0");
+        }
+        scanner.close();
+    }
+}
