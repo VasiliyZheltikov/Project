1bc31486ebbd534c36b0e1827e3684a97554de6a
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-05-16 10:40:45 +0300

    Homework_8 Inputed strings' sorter done

--- src/homework_8/Main.java
+++ src/homework_8/Main.java
@@ -1,5 +1,6 @@
 package homework_8;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -10,9 +11,10 @@ public class Main {
         String[] mass = new String[]{scanner.nextLine(), scanner.nextLine(), scanner.nextLine()};
 
         findShortestAndLongest(mass);
+        sortedUp(mass);
     }
 
-    public static void findShortestAndLongest(String[] mass) {
+    public static void findShortestAndLongest(String[] mass) { // ЗАДАЧА 1
         String shortest = mass[0];
         String longest = mass[0];
         for (String s : mass) {
@@ -24,5 +26,19 @@ public class Main {
         }
         System.out.printf("Самая короткая строка: \"%s\" имеет длину: %s%n", shortest, shortest.length());
         System.out.printf("Самая длинная строка: \"%s имеет длину: %s%n", longest, longest.length());
+    }
+
+    public static void sortedUp(String[] mass) {
+        String temp;
+        for (int i = 0; i < mass.length - 1 ; i++) {
+            for (int j = i + 1; j < mass.length; j++) {
+                if (mass[j].length() < mass[i].length()) {
+                    temp = mass[i];
+                    mass[i] = mass[j];
+                    mass[j] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(mass));
     }
 }
