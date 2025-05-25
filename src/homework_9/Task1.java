30941da375f7d098681d54aac5cbb0043f24e854
Vasiliy Zheltikov <zholtikov1997@yandex.ru>
2025-05-25 15:18:32 +0300

    Homework_9 Task1 DONE

---
+++ src/homework_9/Task1.java
@@ -0,0 +1,16 @@
+package homework_9;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        System.out.print("Введите набор чисел: ");
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        HashSet<String> distinctValues = new HashSet<>(Arrays.asList(inputString.split("")));
+        System.out.println(distinctValues);
+    }
+}
